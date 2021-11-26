26a2be5c97550a7730d1e6f3748a9ff4418e13bd
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2021-11-25 19:47:20 -0800

    base for next app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
-import Amplify from 'aws-amplify'
-import config from '../src/aws-exports'
 
-Amplify.configure({...config, ssr: true })
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

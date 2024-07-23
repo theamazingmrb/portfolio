61c8bba7d54ccb752ed7da27aeb970c00c309f97
theamazingmrb <billie.heidelberg.jr@gmail.com>
2024-07-22 23:30:19 -0700

    fixing linting errors

--- components/AnimatedSection.tsx
+++ components/AnimatedSection.tsx
@@ -28,13 +28,14 @@ const AnimatedSection: React.FC<AnimatedSectionProps> = ({
       { threshold: 0.1 }
     );
 
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
+    const currentRef = sectionRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, [animationType]);
